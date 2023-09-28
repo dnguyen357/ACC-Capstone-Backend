991d4687aae7ede98ed63287424532e3ce0f04c9
daonguyen <dn6226@gmail.com>
2023-09-28 17:55:54 -0400

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express') //bring express in
 const colors = require('colors') 
+const cors = cors();
 const dotenv = require('dotenv').config() //have environment variables 
 const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
@@ -9,7 +10,7 @@ const app = express() //initialize express
 connectDB()
 app.use(express.json())
 app.use(express.urlencoded({ extended:false}))
-
+app.use(cors())
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/products', require('./routes/productRoutes'))
 app.use('/api/carts', require('./routes/cartRoutes'))
