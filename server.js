547e474cbd0837e1044efe2cd75eb057002b3ce7
dnguyen357 <126422056+dnguyen357@users.noreply.github.com>
2023-09-28 20:21:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const connectDB = require('./config/db')
 const app = express() //initialize express
 app.use(cors())
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin: http://localhost:5173', '*');
   next();
 });
 connectDB()
