94363c680317d0efc328084edc00f5b0bff81b9a
dnguyen357 <126422056+dnguyen357@users.noreply.github.com>
2023-09-28 20:18:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 5000
 const connectDB = require('./config/db')
 const app = express() //initialize express
 app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 connectDB()
 app.use(express.json())
 app.use(express.urlencoded({ extended:false}))
