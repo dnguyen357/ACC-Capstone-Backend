b25a2c1b2eabf4bbf6373777ca74371ada75182b
daonguyen <dn6226@gmail.com>
2023-09-28 20:27:19 -0400

    third

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 const connectDB = require('./config/db')
 const app = express() //initialize express
-
+app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 connectDB()
+
 app.use(express.json())
 app.use(express.urlencoded({ extended:false}))
-app.use(cors())
+
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/products', require('./routes/productRoutes'))
 app.use('/api/carts', require('./routes/cartRoutes'))
